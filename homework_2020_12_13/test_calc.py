9f80f319621a18ff5d91e70e1bddac28fe096466
Levine07 <zhangyanji1993@outlook.com>
2020-12-15 12:48:07 +0800

    Initial commit

--- homework_2020_12_13/test_calc.py
+++ homework_2020_12_13/test_calc.py
@@ -1,34 +1,38 @@
 import pytest
-from homework_2020_12_09.pythoncode import calculator
+import yaml
+from .pythoncode.calculator import *
+import os
 
-c = calculator.Calculator()
+c = Calculator()
+datas = yaml.safe_load(open("./datas.yml"))  # 执行效率角度，封装效率较低。采用全局变量
 
 
+@pytest.mark.usefixtures("module_run")
 class TestCal(object):
-    @classmethod
-    def setup_class(cls):
-        print("开始测试\n")
+    # @classmethod
+    # def setup_class(cls):
+    #     print("开始测试\n")
+    #
+    # @classmethod
+    # def teardown_class(cls):
+    #     print("\n结束测试")
 
-    @classmethod
-    def teardown_class(cls):
-        print("\n结束测试")
-
-    @pytest.mark.parametrize("a, b, expect", [
-        (3, 5, 8), (-1, -2, -3), (100, 300, 400), (3.2, 3.3, 6.5)], ids=["int", "minus", "large number", "point"])
+    @pytest.mark.run(order=1)
+    @pytest.mark.parametrize("a, b, expect", datas["add"], ids=datas["tag_name"])
     def test_add(self, a, b, expect):
         assert c.add(a, b) == expect
 
-    @pytest.mark.parametrize("a, b, expect", [
-        (8, 5, 3), (-3, -2, -1), (400, 300, 100), (3.3, 3.2, 0.1)], ids=["int", "minus", "large number", "point"])
+    @pytest.mark.run(order=2)
+    @pytest.mark.parametrize("a, b, expect", datas["sub"], ids=datas["tag_name"])
     def test_sub(self, a, b, expect):
         assert c.sub(a, b) == expect
 
-    @pytest.mark.parametrize("a, b, expect", [
-        (3, 5, 15), (-3, -2, 6), (100, 300, 30000), (3, 3.2, 9.6)], ids=["int", "minus", "large number", "point"])
+    @pytest.mark.run(order=3)
+    @pytest.mark.parametrize("a, b, expect", datas["mul"], ids=datas["tag_name"])
     def test_mul(self, a, b, expect):
         assert c.mul(a, b) == expect
 
-    @pytest.mark.parametrize("a, b, expect", [
-        (15, 5, 3), (6, -2, -3), (100, 4, 25), (9.6, 3, 3.2)], ids=["int", "minus", "large number", "point"])
+    @pytest.mark.run(order=4)
+    @pytest.mark.parametrize("a, b, expect", datas["div"], ids=datas["tag_name"])
     def test_div(self, a, b, expect):
         assert c.div(a, b) == expect
